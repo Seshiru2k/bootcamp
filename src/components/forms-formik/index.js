799c2832c664b3c4f6cbd1c8050e9440c1bb59f7
Seshiru <ceciladam04@gmail.com>
2023-04-24 22:36:04 +0800

    post edit delete

--- src/components/forms-formik/index.js
+++ src/components/forms-formik/index.js
@@ -54,7 +54,7 @@ const SignupForm = () => {
                   last_name: passValue?.lastName,
                   credentials: passValue?.email,
                   password: passValue?.password,
-                  birthdate: passValue?.date,
+                  birthdate: passValue?.birthdate,
                   gender: passValue?.gender,
                   pronoun: passValue?.pronouns,
               })
@@ -76,7 +76,7 @@ const SignupForm = () => {
                   last_name: passValue?.lastName,
                   credentials: passValue?.email,
                   password: passValue?.password,
-                  birthdate: passValue?.date,
+                  birthdate: passValue?.birthdate,
                   gender: passValue?.gender,
                   pronoun: passValue?.pronouns,
               })
@@ -108,15 +108,16 @@ const SignupForm = () => {
         validationSchema={SignupSchema}
         onSubmit={(values) => {
           postPosts(values)
-          setDisplay(`Welcome ${values.firstName} ${values.contactNumber} ${values.lastName}! Your email is ${values.email}, birthdate is ${values.birthMonth} ${values.birthdate} ${values.birthYear}, and gender is ${values.gender}.`);
+          setDisplay(`Welcome ${values.firstName} ${values.lastName} ${values.contactNumber} ! Your email is ${values.email}, birthdate is ${values.birthMonth} ${values.birthdate} ${values.birthYear}, and gender is ${values.gender}.`);
           actions.resetForm({
             values: {
-                id: "",
                 firstName: "",
                 lastName: "",
                 email: "",
                 password: "",
-                date: "",
+                birthdate: "",
+                birthMonth:"",
+                birthYear:"",
                 gender: "",
                 pronouns: "",
                 customGender: "",
@@ -209,11 +210,9 @@ const SignupForm = () => {
             <ErrorMessage name="gender" className={styles["form-error"]}/>
 
             <button type="submit" className={styles["form-submit"]}>Sign Up</button>
-
-            {display && <p className={styles.success}>{display}</p>}
-            <p>{display}</p>
-              <p className={styles.status}>
-                Status:{" "}
+            <br></br>
+            <p style={styles.p}>{display}</p>
+              <p>postStatus:{" "}
                 {postStatus === 200 ? (
                 <span style={{ color: "green" }}>Success</span>
                 ) : postStatus === 500 ? (
@@ -225,7 +224,194 @@ const SignupForm = () => {
           </Form>
         )}
       </Formik>
+      <br></br>
+      <div>
+      <h3>EDIT</h3>
+      <Formik
+        initialValues={{
+          id: "",
+          firstName: "",
+          lastName: "",
+          contactNumber: "",
+          email: "",
+          password: "",
+          birthdate: "",
+          birthMonth:"",
+          birthYear:"",
+          gender: "",
+          customGender: "",
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={(values) => {
+          patchPosts(values)
+          setDisplay(`Welcome ${values.firstName} ${values.lastName} ${values.contactNumber} ! Your email is ${values.email}, birthdate is ${values.birthMonth} ${values.birthdate} ${values.birthYear}, and gender is ${values.gender}.`);
+          actions.resetForm({
+            values: {
+                id: "",
+                firstName: "",
+                lastName: "",
+                contactNumber: "",
+                email: "",
+                password: "",
+                birthdate: "",
+                birthMonth:"",
+                birthYear:"",
+                gender: "",
+                pronouns: "",
+                customGender: "",
+            },
+        })
+    }}
+      >
+        {({ errors, touched, values }) => (
+          <Form className={styles["form-container"]}>
+            <h2>Sign Up!</h2>
+            <br>
+            </br>
+            <h4>It's quick and easy</h4>
+            <br>
+            </br>
+            <Field style={styles.input} id="id" name="id" placeholder="ID number" />
+            <label htmlFor="firstName" className={styles["form-label"]} >First Name</label>
+            <Field id="firstName" name="firstName" placeholder="First Name" className={styles["form-input"]}/>
+            <ErrorMessage name="firstName" className={styles["form-error"]} />
+
+            <label htmlFor="lastName"className={styles["form-label"]} >Last Name</label>
+            <Field id="lastName" name="lastName" placeholder="Last Name" className={styles["form-input"]} />
+            <ErrorMessage name="lastName" className={styles["form-error"]} />
+
+            <label htmlFor="contactNumber" className={styles["form-label"]}>Contact Number</label>
+            <Field id="contactNumber" name="contactNumber" placeholder="Contact Number" className={styles["form-input"]} />
+            <ErrorMessage name="contactNumber" className={styles["form-error"]} />
+
+
+            <label htmlFor="email" className={styles["form-label"]} >Email</label>
+            <Field id="email" name="email" placeholder="Email" className={styles["form-input"]} />
+            <ErrorMessage name="email" className={styles["form-error"]} />
+
+            <label htmlFor="password" className={styles["form-label"]} >Password</label>
+            <Field
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Password"
+              className={styles["form-input"]}
+            />
+            <ErrorMessage name="password" className={styles["form-error"]} />
+            <br>
+            </br>
+
+            <label htmlFor="birthdate">Birthdate</label>
+            <Field as="select" id="birthdate" name="birthdate" className={styles["form-input"]}>
+              <option value="">Select</option>
+            {Array.from({ length: 31 }, (_, i) => i + 1).map((day) => (
+              <option key={day} value={day}>
+            {day}
+            </option>
+            ))}
+            </Field>
+            <Field as="select" id="birthMonth" name="birthMonth" className={styles["form-input"]}>
+              <option value="">Month</option>
+              <option value="January">January</option>
+              <option value="February">February</option>
+              <option value="March">March</option>
+              <option value="April">April</option>
+              <option value="May">May</option>
+              <option value="June">June</option>
+              <option value="July">July</option>
+              <option value="August">August</option>
+              <option value="September">September</option>
+              <option value="October">October</option>
+              <option value="November">November</option>
+              <option value="December">December</option>
+            </Field>
+            <Field as="select" id="birthYear" name="birthYear" className={styles["form-input"]}>
+              <option value="">Year</option>
+              {Array.from({ length: 100 }, (_, i) => new Date().getFullYear() - i).map((year) => (
+              <option key={year} value={year}>
+              {year}
+              </option>
+              ))}
+            </Field>
+            <ErrorMessage name="birthdate" className={styles["form-error"]} />
+            <br>
+            </br>
+
+
+            <label htmlFor="gender">Gender</label>
+            <Field as="select" id="gender" name="gender">
+              <option value="">Select</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="custom">Custom</option>
+              className={styles["form-input"]}
+            </Field>
+            <ErrorMessage name="gender" className={styles["form-error"]}/>
+
+            <button type="submit" className={styles["form-submit"]}>Sign Up</button>
+            <br></br>
+              <p style={styles.p}>{display}</p>
+              <p>
+                patchStatus:{" "}
+                {patchStatus === 200 ? (
+                <span style={{ color: "green" }}>Success</span>
+                ) : patchStatus === 500 ? (
+                <span style={{ color: "red" }}>Failed</span>
+                ) : (
+                "N/A"
+                )}
+              </p>
+          </Form>
+        )}
+      </Formik>       
+      </div>
+
+      <div>
+      <br/>
+        <p>DELETE</p>
+        <Formik
+          initialValues={{
+            id: "",
+          }}
+          onSubmit={(values) => {
+            deletePosts(values);
+          }}
+        >
+          {({ errors, touched, values }) => (
+            <Form>
+              <label style={styles.label} htmlFor="id">ID </label>
+              <Field style={styles.input} id="id" name="id" placeholder="ID" />
+              <ErrorMessage style={{ color: "red" }} name="id" />
+              <br />
+              <button style={styles.signUpButton} type="submit">Delete</button>
+              <br/>
+              <p>
+                Delete Status:{" "}
+                {deleteStatus === 200 ? (
+                  <span style={{ color: "green" }}>Success</span>
+                ) : deleteStatus === 500 ? (
+                  <span style={{ color: "red" }}>Failed</span>
+                ) : (
+                  "N/A"
+                )}
+              </p>
+            </Form>
+          )}
+        </Formik>
+      <br/>
+        <div>
+        <p>GET</p>
+        {data && data.map((value, index) => {
+            return (
+              <p key={index}>
+                {index}: {value?.first_name}
+              </p>
+            );
+          })}
+      </div>
+      </div>
     </div>
+
     
   );
 };
